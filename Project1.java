44f18ea2d2bfae0e70767c5f1cee0e0fd20f72d0
Phrosyne <mcoderej@gmail.com>
2026-09-23 18:54:20 -0700

    init commit

---
+++ Project1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Project1 {
+    Scanner input;
+    Process[] table;
+    public static void main(String[] args) {
+        
+    }
+
+    public static void printHierarchy() {
+
+    }
+
+    public static void addProcess() {
+        System.out.println("Enter the parent process index for the child process:");
+    }
+}
